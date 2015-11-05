ec95284b010a5ce2c05c008f0740d704490db43b
Martin Cole <teassy000@gmail.com>
2015-11-05 17:58:51 +0800

    modify light effect.
    modify light effect.

--- headers/light.h
+++ headers/light.h
@@ -27,22 +27,23 @@ struct MaterialInfo
 // initialize light to a fixed value.
 void InitLight(LightInfo &light_info)
 {
-	light_info.position = glm::vec4(0.0f, 0.0f, 20.0f, 1.0f);
+	// light should be far from object to get a good light effect.
+	light_info.position = glm::vec4(200.0f, 10.0f, -2.0f, 1.0f);
 	// default light is white.
 	light_info.color = glm::vec4(1.0f);
 
-	light_info.Ld = vec3(0.01f);
-	light_info.La = vec3(0.3f);
+	light_info.Ld = vec3(0.3f);
+	light_info.La = vec3(0.4f);
 	light_info.Ls = vec3(0.3f);
 }
 
 void InitMaterial(MaterialInfo &material_info)
 {
-	material_info.Kd = vec3(0.05f);
-	material_info.Ka = vec3(0.1f);
+	material_info.Kd = vec3(0.3f);
+	material_info.Ka = vec3(0.4f);
 	material_info.Ks = vec3(0.3f);
 
-	material_info.shininess = 0.0f;
+	material_info.shininess = 0.9f;
 }
 
 #endif
