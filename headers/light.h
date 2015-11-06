eda91e873806352553e4a0f9f97a8dd9e4c3bb7d
Martin Cole <teassy000@gmail.com>
2015-11-06 16:23:27 +0800

    Modify light configuration.

--- headers/light.h
+++ headers/light.h
@@ -9,8 +9,6 @@
 struct LightInfo
 {
 	glm::vec4		position;
-	glm::vec4		color;
-
 	glm::vec3		Ld;		// Diffuse intensity
 	glm::vec3		La;		// Ambient intensity
 	glm::vec3		Ls;		// Specular	intensity
@@ -27,23 +25,19 @@ struct MaterialInfo
 // initialize light to a fixed value.
 void InitLight(LightInfo &light_info)
 {
-	// light should be far from object to get a good light effect.
-	light_info.position = glm::vec4(200.0f, 10.0f, -2.0f, 1.0f);
-	// default light is white.
-	light_info.color = glm::vec4(1.0f);
-
-	light_info.Ld = vec3(0.3f);
-	light_info.La = vec3(0.4f);
-	light_info.Ls = vec3(0.3f);
+	light_info.position = glm::vec4(600.0f, 10000, 40.0f, 1.0f);
+	light_info.Ld = vec3(0.4f, 0.5f, 0.2f);
+	light_info.La = vec3(0.4f, 0.5f, 0.2f);
+	light_info.Ls = vec3(0.4f, 0.5f, 0.2f);
 }
 
 void InitMaterial(MaterialInfo &material_info)
 {
-	material_info.Kd = vec3(0.3f);
-	material_info.Ka = vec3(0.4f);
-	material_info.Ks = vec3(0.3f);
+	material_info.Kd = vec3(0.4f, 0.5f, 0.2f);
+	material_info.Ka = vec3(0.4f, 0.5f, 0.2f);
+	material_info.Ks = vec3(0.4f, 0.5f, 0.2f);
 
-	material_info.shininess = 0.9f;
+	material_info.shininess = 0.3f;
 }
 
 #endif
